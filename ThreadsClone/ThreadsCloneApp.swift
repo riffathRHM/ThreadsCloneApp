18e0a05c15035c62f1634531a4cb2c99d40ba1f6
riffathRHM <riffath1998@gmail.com>
2024-08-06 09:19:11 +0530

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone 
+// Created by Mohamed Riffath on  8/6/24.
+
+
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
